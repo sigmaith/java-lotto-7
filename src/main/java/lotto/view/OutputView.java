ac0c4e5db6e509b7310cd2155f7b367850ad7362
Sumi Chung <drsoombre@naver.com>
2024-12-01 16:53:32 +0900

    fix: % 이스케이프 문자 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,6 @@
 package lotto.view;
 
-import java.util.HashMap;
+import java.util.Map;
 import lotto.model.Lottoes;
 import lotto.model.Money;
 import lotto.util.Rank;
@@ -8,7 +8,7 @@ import lotto.util.Rank;
 public class OutputView {
     private static final String PURCHASED_LOTTOES_GUIDE = "%d개를 구매했습니다.";
     private static final String WINNING_STATISTICS = "당첨 통계\n---";
-    private static final String EARNING_RATES = "총 수익률은 %.2f입니다.";
+    private static final String EARNING_RATES = "총 수익률은 %.1f%%입니다.";
 
     public static void printPurchasedLottoes(Lottoes lottoes) {
         System.out.println();
@@ -16,16 +16,16 @@ public class OutputView {
         System.out.println(lottoes.toString());
     }
 
-    public static void printWinningStatistics(HashMap<Integer, Integer> matchingRecords) {
+    public static void printWinningStatistics(Map<Integer, Integer> matchingRecords) {
         System.out.println(WINNING_STATISTICS);
         StringBuilder sb = new StringBuilder();
         for (int i = 3; i <= 7; i++) {
-            sb.append(Rank.getRankByCode(i).getDescription()).append(matchingRecords.get(i));
+            sb.append(Rank.getRankByCode(i).getDescription()).append(matchingRecords.get(i)).append("개\n");
         }
         System.out.println(sb.toString());
     }
 
-    public static void printEarningRates(HashMap<Integer, Integer> matchingRecords, Money money) {
+    public static void printEarningRates(Map<Integer, Integer> matchingRecords, Money money) {
         int earnings = 0;
         for (int i = 3; i <= 7; i++) {
             earnings += matchingRecords.get(i) * Rank.getRankByCode(i).getWinningMoney();
